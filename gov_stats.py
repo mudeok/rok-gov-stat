644558189a9055ba60c79bc5be0d4c79af7c491f
mudeok <mudeok.rok@gmail.com>
2022-09-24 14:16:47 +0200

    refactor(gov_stats): Cleans a bit

--- gov_stats.py
+++ gov_stats.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from operator import invert
 from typing import List
 from collections import namedtuple
 import pyexcel as pe
@@ -15,36 +14,23 @@ OCR_LOCATION = namedtuple("ORC_LOCATION", ["id", "bbox", "number"])
 TESSERACT_ARGS_NUMBER = "--oem 3 --psm 7 digits -c tessedit_char_whitelist=0123456789"
 TESSERACT_ARGS_TEXT = "--oem 3 --psm 7"
 
-id_name_power_killpoints = [
-    OCR_LOCATION("id", (1630, 774, 140, 33), True),
-    OCR_LOCATION("name", (1449, 806, 634, 58), False),
-    OCR_LOCATION("power", (1798, 940, 279, 43), True),
-    OCR_LOCATION("kill_points", (2088, 939, 286, 40), True),
-]
 
 name_dead = [
-    OCR_LOCATION("name", (1118, 628, 466, 85), False),
-    OCR_LOCATION("power", (1676, 634, 326, 68), True),
-    OCR_LOCATION("kills_points", (2194, 629, 262, 80), True),
-    OCR_LOCATION("dead", (1732, 1050, 609, 45), True),
-    OCR_LOCATION("ressource_gathered", (1776, 1263, 571, 58), True),
-    OCR_LOCATION("ressource_assistance", (1776, 1341, 571, 58), True),
-    OCR_LOCATION("alliance_help_times", (1776, 1424, 571, 58), True),
+    OCR_LOCATION("name", (1142, 675, 462, 95), False),
+    OCR_LOCATION("power", (1701, 675, 325, 95), True),
+    OCR_LOCATION("kills_points", (2216, 675, 258, 95), True),
+    OCR_LOCATION("dead", (1789, 1088, 570, 65), True),
+    OCR_LOCATION("ressource_gathered", (1789, 1313, 570, 65), True),
+    OCR_LOCATION("ressource_assistance", (1789, 1394, 570, 65), True),
+    OCR_LOCATION("alliance_help_times", (1789, 1475, 570, 65), True),
 ]
 
 id_kills = [
-    OCR_LOCATION("id", (1628, 770, 142, 37), True),
-    OCR_LOCATION("kills_t3", (1739, 1353, 331, 49), True),
-    OCR_LOCATION("kill_points_t3", (2121, 1356, 352, 45), True),
-    OCR_LOCATION("kills_t4", (1739, 1415, 331, 49), True),
-    OCR_LOCATION("kill_points_t4", (2121, 1417, 352, 45), True),
-    OCR_LOCATION("kills_t5", (1739, 1474, 331, 49), True),
-    OCR_LOCATION("kill_points_t5", (2121, 1477, 352, 45), True),
+    OCR_LOCATION("id", (1650, 822, 145, 41), True),
+    OCR_LOCATION("kills_t3", (1763, 1404, 336, 52), True),
+    OCR_LOCATION("kills_t4", (1763, 1464, 336, 52), True),
+    OCR_LOCATION("kills_t5", (1763, 1523, 336, 52), True),
 ]
-
-folder_id_name_power_killpoints = "rok_ss_step_1"
-folder_name_dead = "rok_ss_step_2"
-folder_id_kills = "rok_ss_step_3"
 
 
 def get_grayscale(image):
@@ -93,36 +79,6 @@ def get_text(image, locations: List[OCR_LOCATION]) -> List[str]:
     return values
 
 
-def parse_folder(folder: str, locations: List[OCR_LOCATION]) -> List[List[str]]:
-    """ Parse a folder to retrieve all screenshots inside that folder.
-    Then iterate through all screenshot to extract data.
-    Return a list of values extracted for each screenshots.
-    """
-    folder_path = os.path.abspath(folder)
-    files = os.listdir(folder_path)
-    files_values = []
-
-    for a_file in files:
-        filename, extension = os.path.splitext(a_file)
-
-        # hack, we skip macos hidden file
-        if filename == ".DS_Store":
-            continue
-        elif extension != ".png" \
-                and extension != ".jpeg" and \
-                extension != ".jpg":
-            continue
-
-        file_path = os.path.abspath(f"{folder}/{a_file}")
-        # we treat the file only if it exists
-        if os.path.exists(file_path):
-            image = cv2.imread(file_path)
-            values = get_text(image, locations)
-            files_values += [values]
-
-    return files_values
-
-
 def save_data_into_file(filename, data):
     # Save data into an .xlsx file
     d_now = datetime.utcnow()
@@ -133,109 +89,7 @@ def save_data_into_file(filename, data):
     pe.save_as(array=data, dest_file_name=file_destination)
 
 
-def get_data(headers: List[str], filename: str, folder_name: str, locations: List[OCR_LOCATION], save=True):
-    export_content = []
-    export_content.append(headers)
-    screenshots = parse_folder(folder_name, locations)
-    export_content += [screenshot for screenshot in screenshots]
-    if save is True:
-        save_data_into_file(filename, export_content)
-    return screenshots
-
-
-def get_id_name_power_kill_points(folder_name: str, locations: List[OCR_LOCATION], save=True):
-    export_header = ["ID", "NAME", "POWER", "KILL POINTS"]
-    return get_data(
-        export_header,
-        "rok_gov_id_name_power_killpts",
-        folder_name,
-        locations,
-        save=save
-    )
-
-
-def get_data_name_deads(folder_name: str, locations: List[OCR_LOCATION], save=True):
-    export_header = ["NAME", "DEAD"]
-    return get_data(
-        export_header,
-        "rok_gov_name_deads",
-        folder_name,
-        locations,
-        save=save
-    )
-
-
-def get_data_id_kills(folder_name: str, locations: List[OCR_LOCATION], save=True):
-    export_header = ["ID", "KILLS T3", "KILL POINTS T3", "KILLS T4", "KILL POINTS T4", "KILLS T5", "KILL POINTS T5"]
-    return get_data(
-        export_header,
-        "rok_gov_name_deads",
-        folder_name,
-        locations,
-        save=save
-    )
-
-
-def aggregate_data(data_1: List[List[str]], data_2: List[List[str]], data_3: List[List[str]]):
-    export_header = [
-        "ID",
-        "NAME",
-        "POWER",
-        "KILL POINTS",
-        "DEAD",
-        "KILLS T3",
-        "KILL POINTS T3",
-        "KILLS T4",
-        "KILL POINTS T4",
-        "KILLS T5",
-        "KILL POINTS T5",
-    ]
-    export_content = []
-    export_content.append(export_header)
-
-    memory_2 = {}
-    for part in data_2:
-        # part[0] refer to governor name
-        # part[1] refer to governor number of dead
-        memory_2[part[0]] = part[1]
-
-    memory_3 = {}
-    for part in data_3:
-        # part[0] refer to governor id
-        # part[1:] refer to governor kills stats
-        memory_3[part[0]] = part[1:]
-
-    for part in data_1:
-        a_copy = part.copy()
-        id_ = a_copy[0]
-        name = a_copy[1]
-
-        # match name with data_2
-        # we add the column with governor dead
-        if name in memory_2.keys():
-            a_copy += [memory_2[name]]
-        else:
-            a_copy += []
-
-        # match id with data_3
-        # we add columns for kills stats (t3, t4, t5)
-        kills_stats = memory_3.get(id_, [])
-        if len(kills_stats) == 6:
-            a_copy += [col for col in kills_stats]
-
-        export_content += [a_copy]
-
-    save_data_into_file("rok_gov_stats", export_content)
-
-
-#p1 = get_id_name_power_kill_points(folder_id_name_power_killpoints, id_name_power_killpoints, save=False)
-#p2 = get_data_name_deads(folder_name_dead, name_dead, save=False)
-#p3 = get_data_id_kills(folder_id_kills, id_kills, save=False)
-#aggregate_data(p1, p2, p3)
-
-
-def get_gov_stats():
-    folder = "rok_gov_ss"
+def get_gov_stats(folder):
     folder_path = os.path.abspath(folder)
     files = sorted(os.listdir(folder_path), reverse=True)
 
@@ -276,7 +130,8 @@ def get_gov_stats():
     return hm_gov
 
 
-data = get_gov_stats()
+folder = "FOLDER_NAME"
+data = get_gov_stats(folder)
 values = list(data.values())
 export_header = [
     "ID",
@@ -288,13 +143,10 @@ export_header = [
     "RSS ASSISTANCE",
     "ALLIANCE HELP",
     "KILLS T3",
-    "KILL POINTS T3",
     "KILLS T4",
-    "KILL POINTS T4",
     "KILLS T5",
-    "KILL POINTS T5",
 ]
 export_content = []
 export_content.append(export_header)
 export_content += values
-save_data_into_file("rok_gov_stats", export_content)
+save_data_into_file(f"rok_gov_stats_{folder}", export_content)
